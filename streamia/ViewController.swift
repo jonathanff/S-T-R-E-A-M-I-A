48cd75afa110a794416d508d889a97d9a019abba
Jonathan Fuentes Flores <jonathan.fuentesf@gmail.com>
2017-03-30 22:39:58 -0600

    Initial Commit

---
+++ streamia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  streamia
+//
+//  Created by Jonathan Fuentes Flores on 3/30/17.
+//  Copyright © 2017 appollo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
